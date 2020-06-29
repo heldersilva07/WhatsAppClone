c500e161761031c2bbe321a04f69328493c83f34
HelderSilva <silvahelder.03@gmail.com>
2020-06-29 11:51:47 +0100

    Eighth commit

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -186,8 +186,6 @@ export class WhatsAppController {
         Message.getRef(this._contactActive.chatId).orderBy('timeStamp')
             .onSnapshot(docs => {
 
-
-
                 let scrollTop = this.el.panelMessagesContainer.scrollTop;
 
                 let scrollTopMax = (this.el.panelMessagesContainer.scrollHeight - this.el.panelMessagesContainer.offsetHeight);
@@ -199,17 +197,30 @@ export class WhatsAppController {
                     let data = doc.data();
                     data.id = doc.id;
 
+                    let message = new Message();
+
+                    message.fromJSON(data);
+
+                    let me = (data.from === this._user.email);
+
                     if (!this.el.panelMessagesContainer.querySelector('#_' + data.id)) {
 
-                        let message = new Message();
+                        if(!me){
 
-                        message.fromJSON(data);
-
-                        let me = (data.from === this._user.email);
+                            doc.ref.set({
+                                status:'read'
+                            },{
+                                merge: true
+                            });
+                        }
                         let view = message.getViewElement(me);
 
                         this.el.panelMessagesContainer.appendChild(view);
 
+                    } else if(me) {
+                        let msgEl = this.el.panelMessagesContainer.querySelector('#_' + data.id);
+
+                        msgEl.querySelector('.message-status').innerHTML = message.getStatusViewElement().outerHTML;
                     }
 
                 });
